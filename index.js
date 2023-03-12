4b4f8ea73f8c3d2dab6f71181884770da8bfbe82
adeniranadeyemo <niranadeniran1@gmail.com>
2023-03-12 19:40:40 +0100

    Added backdrop-filter

--- index.js
+++ index.js
@@ -1,44 +1,50 @@
-const nikeShoes = document.querySelector('.nshoesrow')
-const nikeShoe = document.querySelector('.nshoe')
-const prevButton = document.querySelector('.lbutton')
-const nextButton = document.querySelector('.rbutton')
+const nikeShoes = document.querySelector('.nshoesrow');
+const nikeShoe = document.querySelector('.nshoe');
+const prevButton = document.querySelector('.lbutton');
+const nextButton = document.querySelector('.rbutton');
 
-nextButton.addEventListener("click", (event) => {
+const nShoes = document.querySelector('.nssr');
+const nShoe = document.querySelector('.ns');
+const prvButton = document.querySelector('.lbtn');
+const nxtButton = document.querySelector('.rbtn');
+
+const menu = document.querySelector('.bx-menu');
+const close = document.querySelector('.close');
+const nav = document.querySelector('.nav2');
+const body = document.querySelector('body');
+const overlay = document.querySelector('.overlay');
+
+nextButton.addEventListener('click', event => {
   const nikeShoeWidth = nikeShoe.clientWidth;
-    nikeShoes.scrollLeft += nikeShoeWidth;
+  nikeShoes.scrollLeft += nikeShoeWidth;
 });
 
-prevButton.addEventListener("click", () => {
+prevButton.addEventListener('click', () => {
   const nikeShoeWidth = nikeShoe.clientWidth;
   nikeShoes.scrollLeft -= nikeShoeWidth;
 });
 
-const nShoes = document.querySelector('.nssr')
-const nShoe = document.querySelector('.ns')
-const prvButton = document.querySelector('.lbtn')
-const nxtButton = document.querySelector('.rbtn')
-
-nxtButton.addEventListener("click", (event) => {
+nxtButton.addEventListener('click', event => {
   const nShoeWidth = nShoe.clientWidth;
-    nShoes.scrollLeft += nShoeWidth;
+  nShoes.scrollLeft += nShoeWidth;
 });
 
-prvButton.addEventListener("click", () => {
+prvButton.addEventListener('click', () => {
   const nShoeWidth = nShoe.clientWidth;
   nShoes.scrollLeft -= nShoeWidth;
 });
 
-const menu = document.querySelector('.bx-menu')
-const close = document.querySelector('.close')
-const nav = document.querySelector('.nav2')
-const body = document.querySelector('body')
-
 menu.addEventListener('click', () => {
-  nav.classList.add('open-nav')
-  nav.classList.add('blur')
-})
+  nav.classList.add('open-nav');
+  overlay.classList.add('open-overlay');
+});
 
 close.addEventListener('click', () => {
-  nav.classList.remove('open-nav')
-  nav.classList.remove('blur')
-})
+  nav.classList.remove('open-nav');
+  overlay.classList.remove('open-overlay');
+});
+
+overlay.addEventListener('click', () => {
+  nav.classList.remove('open-nav');
+  overlay.classList.remove('open-overlay');
+});
