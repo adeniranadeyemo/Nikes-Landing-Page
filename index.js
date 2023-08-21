f9c08926b7e943ec3264f437eecbb81aee36a8e5
adeniranadeyemo <niranadeniran1@gmail.com>
2023-08-21 15:11:13 +0100

    Update

--- index.js
+++ index.js
@@ -1,47 +1,45 @@
-const nikeShoes = document.querySelector('.nshoesrow');
-const nikeShoe = document.querySelector('.nshoe');
-const prevButton = document.querySelector('.lbutton');
-const nextButton = document.querySelector('.rbutton');
-
-// const nBtnArray = Array.from(nextButton);
-
-// nBtnArray.forEach(nbtn =>
-//   nbtn.addEventListener('click', () => {
-//     const nikeShoeWidth = nikeShoe.clientWidth;
-//     nikeShoes.scrollLeft += nikeShoeWidth;
-//   })
-// );
-
-nextButton.addEventListener('click', () => {
-  const nikeShoeWidth = nikeShoe.clientWidth;
-  nikeShoes.scrollLeft += nikeShoeWidth;
-});
-
-prevButton.addEventListener('click', () => {
-  const nikeShoeWidth = nikeShoe.clientWidth;
-  nikeShoes.scrollLeft -= nikeShoeWidth;
-});
-
+const body = document.querySelector('body');
+//
+const nikeShoesCon = document.querySelector('.nshoesrow');
+const carouselInner = document.querySelector('.carousel-inner');
+const nikeShoes = document.querySelectorAll('.nshoe');
+const prevButtons = document.querySelectorAll('.lbutton');
+const nextButtons = document.querySelectorAll('.rbutton');
+const carousels = document.querySelectorAll('.carousel-inner');
+//
+const nikeShoesArray = Array.from(nikeShoes);
+const prevButtonsArray = Array.from(prevButtons);
+const nextButtonsArray = Array.from(nextButtons);
+const carouselsArray = Array.from(carousels);
+//
 const nShoes = document.querySelector('.nssr');
 const nShoe = document.querySelector('.ns');
-const prvButton = document.querySelector('.lbtn');
-const nxtButton = document.querySelector('.rbtn');
-
-nxtButton.addEventListener('click', () => {
-  const nShoeWidth = nShoe.clientWidth;
-  nShoes.scrollLeft += nShoeWidth;
-});
-
-prvButton.addEventListener('click', () => {
-  const nShoeWidth = nShoe.clientWidth;
-  nShoes.scrollLeft -= nShoeWidth;
-});
-
+//
 const menu = document.querySelector('.bx-menu');
 const close = document.querySelector('.close');
 const nav = document.querySelector('.nav2');
-const body = document.querySelector('body');
 const overlay = document.querySelector('.overlay');
+//
+
+nextButtonsArray.forEach((nextButton, index) => {
+  nextButton.addEventListener('click', function () {
+    const carousel = carouselsArray[index];
+    const shoe = nikeShoesArray[0];
+
+    const nikeShoeWidth = shoe.clientWidth;
+    carousel.scrollLeft += nikeShoeWidth;
+  });
+});
+
+prevButtonsArray.forEach((prevButton, index) => {
+  prevButton.addEventListener('click', function () {
+    const carousel = carouselsArray[index];
+    const shoe = nikeShoesArray[0];
+
+    const nikeShoeWidth = shoe.clientWidth;
+    carousel.scrollLeft -= nikeShoeWidth;
+  });
+});
 
 const openNav = function () {
   nav.classList.add('open-nav');
---
+++ unused.js
@@ -0,0 +1,12 @@
+// let x = 0;
+
+// const fontSizePixels = parseFloat(window.getComputedStyle(body).fontSize);
+
+// const totalWidthPixels = carouselInner.scrollWidth;
+// const totalWidthEm = totalWidthPixels / fontSizePixels;
+
+// const visibleWidthPixels = nikeShoesCon.clientWidth;
+// const visibleWidthEm = visibleWidthPixels / fontSizePixels;
+
+// const hiddenWidthPixels = totalWidthPixels - visibleWidthPixels;
+// const hiddenWidthEm = totalWidthEm - visibleWidthEm;
